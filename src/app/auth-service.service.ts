b6883f53c181eedc943a5f43f0fd0d654bce0dca
Ari Frankel <ari.l.frankel@gmail.com>
2017-02-03 14:23:46 -0600

    auth on open app not with buttons

--- src/app/auth-service.service.ts
+++ src/app/auth-service.service.ts
@@ -1,12 +1,7 @@
 import { Injectable } from '@angular/core';
 
 declare var gapi:any;
-// declare var CLIENT_ID:any;
-// declare var calendarId:any;
-// declare var CalendarUrl:any;
-// declare var ROOMS:any;
-// declare var SCOPES:string[];
-// declare var window:any;
+
 
 @Injectable()
 export class AuthService {
