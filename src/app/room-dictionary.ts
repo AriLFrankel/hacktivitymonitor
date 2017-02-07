7c9e75e8233fa6b68b7dcba80141375544e63c96
Ari Frankel <ari.l.frankel@gmail.com>
2017-02-07 15:16:00 -0600

    add Hamilton to rooms

--- src/app/room-dictionary.ts
+++ src/app/room-dictionary.ts
@@ -6,6 +6,7 @@ export const roomDictionary = {
   'hackreactor.com_3532303334313531373535@resource.calendar.google.com': 'Turing',
   'hackreactor.com_ljtk4epeeca4bm4b73m09cb4c4@group.calendar.google.com': 'Junior',
   'hackreactor.com_9kddcjfdij7ak91o0t2bdlpnoo@group.calendar.google.com': 'Senior',
+  'hackreactor.com_3836363230383730323630@resource.calendar.google.com': 'Hamilton',
   'hackreactor.com_2d3433363932323136393534@resource.calendar.google.com': 'Junior Open Classroom',
   'hackreactor.com_2d3231313833303133383036@resource.calendar.google.com': 'Senior Open Classroom',
   'Djikstra': 'hackreactor.com_2d373931333934353637@resource.calendar.google.com',
@@ -15,6 +16,7 @@ export const roomDictionary = {
   'Turing': 'hackreactor.com_3532303334313531373535@resource.calendar.google.com',
   'Senior': 'hackreactor.com_9kddcjfdij7ak91o0t2bdlpnoo@group.calendar.google.com',
   'Junior': 'hackreactor.com_ljtk4epeeca4bm4b73m09cb4c4@group.calendar.google.com',
+  'Hamilton': 'hackreactor.com_3836363230383730323630@resource.calendar.google.com'
   'Junior Open Classroom' : 'hackreactor.com_2d3433363932323136393534@resource.calendar.google.com',
   'Senior Open Classroom' : 'hackreactor.com_2d3231313833303133383036@resource.calendar.google.com'
 };
