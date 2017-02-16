f739361bd0103384828a1b90907b484b2f39243f
Ari Frankel <ari.l.frankel@gmail.com>
2017-02-16 12:41:59 -0600

    add logo. rearrange center column

--- src/app/hackerspace-component.component.ts
+++ src/app/hackerspace-component.component.ts
@@ -7,9 +7,10 @@ import { Component } from '@angular/core'
     <div class="row">
       <hd-senior-schedule class="col-md-4"></hd-senior-schedule>
       <div class="col-md-4">
-        <hd-rooms></hd-rooms>
         <hd-weather></hd-weather>
         <hd-clock></hd-clock>
+        <hd-rooms></hd-rooms>
+        <img src='http://hrhqdir.s3.amazonaws.com/brand-guide/HackReactor_RGBLogo-White-Blue.png' width="100%">
       </div>
       <hd-junior-schedule class="col-md-4"></hd-junior-schedule> 
     </div>
