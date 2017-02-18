ed89799351f7423a31e817dfd9223798a3734fd3
Caleb Anderson <caleb.anderson130@gmail.com>
2017-02-18 16:10:47 -0600

    /dist

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/dist'));
 
 // const forceSSL = function() {
 //   return function (req, res, next) {
