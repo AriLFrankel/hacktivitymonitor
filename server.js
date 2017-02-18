0dc0b9a8280e8dd739a536c9b0a225751502b1d6
Caleb Anderson <caleb.anderson130@gmail.com>
2017-02-18 16:07:29 -0600

    testing server change

--- server.js
+++ server.js
@@ -4,21 +4,21 @@ const path = require('path');
 
 app.use(express.static(__dirname + '/src'));
 
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
+// const forceSSL = function() {
+//   return function (req, res, next) {
+//     if (req.headers['x-forwarded-proto'] !== 'https') {
+//       return res.redirect(
+//        ['https://', req.get('Host'), req.url].join('')
+//       );
+//     }
+//     next();
+//   }
+// }
 
-app.use(forceSSL());
+// app.use(forceSSL());
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/dist/index.html'));
+// });
 
 app.listen(process.env.PORT || 8000);
